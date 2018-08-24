4a1fe1eb147a970626769a4ebe2e49f282986be1
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-08-23 23:40:12 -0400

    Define function for displaying game over

--- js/main.js
+++ js/main.js
@@ -56,6 +56,8 @@ function init() {
   showWord(words);
   // Call countdown every second
   setInterval(countdown, 1000);
+  // Check game status
+  setInterval(checkStatus, 50);
 }
 
 // Pick & show random word
@@ -78,4 +80,11 @@ function countdown() {
   }
   // Show time
   timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!';
+  }
 }
